c719885bd8d3ffd8a956edd56ce54ba8a9fc620e
Vo Dinh Quan <22521193@gm.uit.edu.vn>
2024-10-17 22:51:50 +0700

    base config

---
+++ fe/src/Pages/ProductDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDetailPage = () => {
+    return (
+        <div>
+            <h1>Product Detail Page Test</h1> {/* Đây là dòng test */}
+        </div>
+    );
+};
+
+export default ProductDetailPage;
