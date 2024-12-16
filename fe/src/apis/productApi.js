67cd82d94e55c7226f8e23d43f773ea3c8661f98
unknown <tranhasonpy@gmail.com>
2024-12-16 12:58:35 +0700

    add search

--- fe/src/apis/productApi.js
+++ fe/src/apis/productApi.js
@@ -1,52 +1,56 @@
 import axiosClient from "./axiosClient";
 
 const productApi = {
-   getAll(params) {
-      const url = "product/";
-      return axiosClient.get(url, { params }); // Truyền params dưới dạng object
-   },
+  getAll(params) {
+    const url = "product/";
+    return axiosClient.get(url, { params }); // Truyền params dưới dạng object
+  },
 
-   get(id) {
-      const url = `product/${id}`;
-      return axiosClient.get(url);
-   },
+  get(id) {
+    const url = `product/${id}`;
+    return axiosClient.get(url);
+  },
 
-   add(data) {
-      const url = "product/";
-      return axiosClient.post(url, data);
-   },
+  add(data) {
+    const url = "product/";
+    return axiosClient.post(url, data);
+  },
 
-   update(data) {
-      const url = `product/${data.id}`;
-      return axiosClient.patch(url, data);
-   },
+  update(data) {
+    const url = `product/${data.id}`;
+    return axiosClient.patch(url, data);
+  },
 
-   remove(id) {
-      const url = `product/${id}`;
-      return axiosClient.delete(url);
-   },
+  remove(id) {
+    const url = `product/${id}`;
+    return axiosClient.delete(url);
+  },
 
-   getRecommendedProducts() {
-      const url = "product/recommend";
-      return axiosClient
-         .get(url)
-         .then((response) => response.data)
-         .catch((error) => {
-            console.error("Error fetching recommendations:", error);
-            throw error;
-         });
-   },
+  getRecommendedProducts() {
+    const url = "product/recommend";
+    return axiosClient
+      .get(url)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching recommendations:", error);
+        throw error;
+      });
+  },
 
-   getRecentProducts() {
-      const url = "product/recent";
-      return axiosClient
-         .get(url)
-         .then((response) => response.data)
-         .catch((error) => {
-            console.error("Error fetching recent products:", error);
-            throw error;
-         });
-   },
+  getRecentProducts() {
+    const url = "product/recent";
+    return axiosClient
+      .get(url)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching recent products:", error);
+        throw error;
+      });
+  },
+  search(query) {
+    const url = "product/search";
+    return axiosClient.get(url, { params: { query } });
+  },
 };
 
 export default productApi;
